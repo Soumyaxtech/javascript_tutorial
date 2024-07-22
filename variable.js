30d3a9edc59ecfd690546ef13b5eca6e75aa8a2e
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-07-22 11:51:22 +0530

    constant and variable in javascript

---
+++ variable.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail = "soumya@google.com"
+var accountPassword = "1364365"
+accountCity = "kolkata"
+let accountState
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity])
+accountCity = "Patna"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
