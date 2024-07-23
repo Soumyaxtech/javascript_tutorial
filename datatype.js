f278abf3a406f78f318f5a3f9134d21975611561
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-07-23 23:58:53 +0530

    diff datatypes in javascript

---
+++ datatype.js
@@ -0,0 +1,26 @@
+"use strict";// TREAT ALL JAVASCRIPT CODE AS NEWER VERSION
+
+//alert(3+2) // HERE WE ARE USING NOTE.JS NOT BROWSER SO CAN'T WRITE LIKE THIS
+
+
+let name = "soumya" // string type
+let age = 23    // int type {number}
+let isLoggedIn = false // boolean
+
+/*  DIFF TYPES OF DATATYPES ..........*/
+
+// number ==> 2 to power 53
+// bigint
+// string ==> ""
+// boolean ==> true/false
+// null ==> standalone value
+// undefined ==> value is not assigned
+// symbol ==> unique
+
+//......................................................
+
+// OBJECT
+
+console.log(typeof "soumya")
+console.log(typeof null)  // TYPE OF NULL IS OBJECT
+console.log(typeof undefined)   // UNDEFINED ITSELF A TYPE
--- variable.js
+++ variable.js
@@ -3,6 +3,10 @@ let accountEmail = "soumya@google.com"
 var accountPassword = "1364365"
 accountCity = "kolkata"
 let accountState
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
 console.log(accountId)
 console.table([accountId,accountEmail,accountPassword,accountCity])
 accountCity = "Patna"
