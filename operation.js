3b1a8bd437fb65b88f6811b036e8835120fb973f
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-07-28 10:17:10 +0530

    all operations in javascript on diff datatype

--- operation.js
+++ operation.js
@@ -1,18 +1,18 @@
-// let value=3
-// let negval=-value
-// console.log(negval) 
-// console.log(2+2)
-// console.log(2-2)
-// console.log(2*2)
-// console.log(2/2)
-// console.log(2%3)
-// console.log(2**3) // it is 2 to the power 3
+let value=3
+let negval=-value
+console.log(negval) 
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%3)
+console.log(2**3) // it is 2 to the power 3
 
 
-// let str1="hello"
-// let str2=" Soumya"
-// let str3=str1+str2
-// console.log(str3)
+let str1="hello"
+let str2=" Soumya"
+let str3=str1+str2
+console.log(str3)
 
 
 
@@ -20,3 +20,8 @@ console.log(1+"3")  // PRINT 13
 console.log("2"+5)  // PRINT 25
 console.log("1"+2+5)    // PRINT 125 AS FIRST STRING
 console.log(2+5+"6")    // PRINT 76 AS NUM FIRST SO (2+5) THEN ADD 6
+
+//TRICKY CONVERTION IT'S NOT GOOD TO DO .......
+
+console.log(+true) // IT GIVES RESULT 1 BUT TRUE+ GIVES ERROR
+console.log(+"")    // IT GIVES VALUE 0
