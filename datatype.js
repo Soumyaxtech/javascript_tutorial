5d3782b83624a10be57d52ef0b96b29b5bb4fe68
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-07-31 19:53:54 +0530

    Non-premitive datatype addeed in this file

--- datatype.js
+++ datatype.js
@@ -6,6 +6,10 @@
 let name = "soumya" // string type
 let age = 23    // int type {number}
 let isLoggedIn = false // boolean
+let temp=null;      // null
+let userEmail;      // undefined type
+const id = Symbol(351765186);       // Symbol type
+const bigNumber=564675876745n;
 
 /*  DIFF TYPES OF DATATYPES ..........*/
 
@@ -24,3 +28,24 @@ let isLoggedIn = false // boolean
 console.log(typeof "soumya")
 console.log(typeof null)  // TYPE OF NULL IS OBJECT
 console.log(typeof undefined)   // UNDEFINED ITSELF A TYPE
+
+
+/* NON-PREMITIVE DATATYPES AND THERE DECLEARATION............*/
+
+// EXAMPLE : ARRAY,OBJECT,FUNCTION
+
+const names = ["soumyajit","koley","babai"]     // ARRAY DECLEARATION
+console.log(names)
+
+let myObj={
+    roll: 157,      // OBJECT IS DECLEARED IN BW CURLY BRACES
+    class: "CSE3",
+
+}
+
+const myFunction= function(){
+    console.log("hello world")
+}
+
+
+console.log(typeof myFunction);
