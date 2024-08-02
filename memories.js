44398fd166af9fa11e4d19eb70e398602c972c39
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-08-02 21:00:06 +0530

    basic discussion about stack and heap memory in javascript

---
+++ memories.js
@@ -0,0 +1,21 @@
+// MEMORIES IN JAVASCRIPT ..................
+
+//      TWO TYPES --> 1.STACK  2.HEAP
+
+let name = "Soumyajit"
+let anotherName = name
+anotherName = "Babai"   // ALL PRIMITIVE DATATYPES STORED IN STACK MEMORY
+console.log(name)       // SO ONLY CHANGE OF COPY OF NAME VARIABLE
+console.log(anotherName)
+
+let userone={
+    email: "user@google.com",
+    id: 1235,
+}
+let usertwo = userone
+usertwo.email="koley@google.com"
+usertwo.id=31656
+console.log(userone.email)
+console.log(usertwo.email) // CHANGE IN MAIN VARIABLE AS STORED IN HEAP
+console.log(userone.id)
+console.log(usertwo.id)
