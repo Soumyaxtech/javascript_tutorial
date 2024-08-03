a0e368d7c5790316ccef5f67d5bbeef5f55e0d3a
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-08-03 09:15:34 +0530

    declearation and diff  operation on string

---
+++ string.js
@@ -0,0 +1,19 @@
+let name = "Soumyajit"
+let id = 98765
+
+
+
+// DECLEARATION OF ARRAY ...................
+
+const game = new String("babai")  // Declearation
+
+
+console.log(game)
+console.log(game[0]) // RETURN 1ST INDEX OF STRING 
+console.log(game.__proto__)     // GIVE PROTOTYPE
+console.log(game.toLowerCase())    // TO GET LOWER CASE OF THE STRING
+console.log(game.toUpperCase())    // TO GET UPPERCASE OF THE STRING
+// OPERATIONS ON STRINGS................
+
+console.log(name+id)    // CONCATINATION BUT IT IS NOT GOOD 
+console.log(`MY NAME IS ${name} AND MY ID ${id}`)   // WE WILL USE IT 
