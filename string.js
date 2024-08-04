c53e494aab9c81643fa7ff88e4db854fe591a28e
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-08-04 11:21:19 +0530

    Updating diff operations and diff functions of string in javascript

--- string.js
+++ string.js
@@ -1,19 +1,42 @@
+
+// DECLEARATION OF ARRAY ...................
 let name = "Soumyajit"
 let id = 98765
 
 
+const game = new String("Koley")  // Declearation
 
-// DECLEARATION OF ARRAY ...................
 
-const game = new String("babai")  // Declearation
+// OPERATIONS ON STRINGS................
+
 
 
 console.log(game)
+console.log(game.length)
 console.log(game[0]) // RETURN 1ST INDEX OF STRING 
 console.log(game.__proto__)     // GIVE PROTOTYPE
 console.log(game.toLowerCase())    // TO GET LOWER CASE OF THE STRING
 console.log(game.toUpperCase())    // TO GET UPPERCASE OF THE STRING
-// OPERATIONS ON STRINGS................
 
 console.log(name+id)    // CONCATINATION BUT IT IS NOT GOOD 
 console.log(`MY NAME IS ${name} AND MY ID ${id}`)   // WE WILL USE IT 
+
+console.log(game.charAt(4)) // give in the index what is the charecter
+
+console.log(game.indexOf('e'))  // give index of the charecter
+
+const sub = game.substring(0,4) // TO CREATE A SUBSTRING FROM STRING
+console.log(sub)
+
+const sliceString = game.slice(1,4)  // TO SLICE A STRING
+console.log(sliceString)
+
+const title = "Sujata"
+console.log(title)
+console.log(title.trim())   // To remove spaces from string
+
+const address = "https:google@45:com"
+console.log(address)
+console.log(address.replace('@','%')) // TO REPLACE A PART FROM A STRING 
+console.log(address.includes('koley'))
+console.log(address.split(':')) // TO SPLIT STRING INTO ARRAY STRING
