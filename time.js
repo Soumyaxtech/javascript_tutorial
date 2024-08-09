cd25c45263e2880cff953e054d51c81e30a65ed5
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-08-09 21:49:59 +0530

    adding more bout  date and time in javascript

---
+++ time.js
@@ -0,0 +1,16 @@
+let date = new Date(2024,7,8)
+console.log(date.getTime())
+let myTimeStand = Date.now()
+console.log(myTimeStand)    // give recent time in millisec from 1971 to now 
+console.log (Math.round((myTimeStand/1000))) // remove decimal value of time
+                    // devide 1000 for milli to sec
+
+let now = new Date()
+console.log(now.getDate())
+console.log(now.getDay())
+console.log(now.getMonth())
+console.log(now.getFullYear())
+console.log(now.toLocaleString('default',{
+    weekday : "long",
+    hour : "2-digit"
+}))
