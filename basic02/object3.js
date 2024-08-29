48eff2494607d152930a240cda3f0810c4cbeff1
Soumyaxtech <129987161+Soumyaxtech@users.noreply.github.com>
2024-08-29 20:10:30 +0530

    destructure of object and basic json added

---
+++ basic02/object3.js
@@ -0,0 +1,26 @@
+// DESTRUCTURE OF OBJECT ..................
+
+const cource = {
+    name : "javascript tutorial",
+    price : 999,
+    teacher : "hitesh choudhury",
+}
+
+const {teacher : tech,price,name} = cource  // deconstruction
+                    // can change variable name in short
+console.log(name)
+console.log(tech)
+console.log(price)
+
+
+// JSON ..........................
+
+//atfirst values comes in form of xml structure now json
+
+/*  {
+    "name" : "soumya",
+    "id" : 4248,
+    "address" : "dhaniakhali"
+}   */
+
+// in json keys and values are all string 
